10e260ee43280d83c68871d39cfcac495bcfa3fc
plastikSun <marcom2k01@gmail.com>
2024-03-27 10:27:55 +0100

    create: user, apartment and booking models and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('rank', ['admin', 'student']);
+            $table->string('address', 50);
+            $table->string('phone', 15);
+            $table->date('birthday');
+            $table->string('IBAN', 30)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
