1f9877abb97a2cc4d4c68f2a52c7bd202e00a6ee
plastikSun <marcom2k01@gmail.com>
2024-03-27 13:04:33 +0100

    add: default values

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Carbon;
 
 return new class extends Migration
 {
@@ -19,10 +20,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('rank', ['admin', 'student']);
-            $table->string('address', 50);
-            $table->string('phone', 15);
-            $table->date('birthday');
+            $table->enum('rank', ['admin', 'landlord', 'student'])->default('student');
+            $table->string('address', 50)->default('Roma 01');
+            $table->string('phone', 15)->default('090330000');
+            $table->date('birthday')->default(Carbon::now());
             $table->string('IBAN', 30)->nullable();
             $table->rememberToken();
             $table->timestamps();
