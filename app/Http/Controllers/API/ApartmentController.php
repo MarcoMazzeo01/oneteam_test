1e29e559cfe4154acda8acaf1c0818bb8f0709c2
plastikSun <marcom2k01@gmail.com>
2024-03-27 12:49:08 +0100

    create: controller, index and store functions

---
+++ app/Http/Controllers/API/ApartmentController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+
+class ApartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $apartments = Apartment::all();
+        return view('guest.apartments', compact('apartments'));
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'address' => 'required|string|max:255',
+            'landlord_id' => 'required|exists:users,id',
+        ]);
+
+        // Create a new apartment record
+        $apartment = Apartment::create([
+            'address' => $validatedData['address'],
+            'landlord_id' => $validatedData['landlord_id'],
+        ]);
+
+        // Save apartment
+        $apartment->save();
+
+        return redirect()->route('apartments.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
